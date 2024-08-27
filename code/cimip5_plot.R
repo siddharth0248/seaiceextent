d2c3df995cee75975cf5cd87611bedaa11196d6b
siddharth0248 <sc0248@uah.edu>
2024-08-27 12:12:04 -0500

    cmip5 plot added for RCP45

---
+++ code/cimip5_plot.R
@@ -0,0 +1,27 @@
+##Plot for CMIP5
+library(dplyr)
+library(lubridate)
+library(ggplot2)
+library(tidyr)
+
+sie_r45_2006.2100 <- sie_r45_2006.2100 %>%
+  mutate(Date = make_date(Year, Month, 1))
+
+sie_r45_2006.2100 <- sie_r45_2006.2100 %>%
+  mutate(across(3:10, ~ . / 10^6))
+
+# Reshape the dataframe to long format
+sie_r45_long <- sie_r45_2006.2100 %>%
+  pivot_longer(cols = c(ACCESS10, ACCESS13, CCSM4, CESM1.CAM5, MIROC.ESM, MIROC.ESM.CHEM, MPI.ESM.LR, MPI.ESM.MR), 
+               names_to = "Model", 
+               values_to = "Value")
+
+
+# Plot using ggplot (1500*1000)
+ggplot(sie_r45_long, aes(x = Date, y = Value)) +
+  geom_line() +
+  geom_vline(xintercept = as.numeric(as.Date("2014-01-01")), color = "red") +
+  labs(x = "Date (Year-Month)", y = "Value") +
+  facet_wrap(~ Model, scales = "free_y") +
+  scale_y_continuous(limits = c(0, 20)) +
+  theme_minimal()
